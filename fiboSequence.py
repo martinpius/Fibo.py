5282951f61f53fa6c3dbd2dc1cc8d8c5cd9d5e79
martinpius <martinpius01@gmail.com>
2020-05-22 22:38:45 +0300

    NewDemo

--- fiboSequence.py
+++ fiboSequence.py
@@ -1,1 +1,18 @@
 print("Demo with Fibonacci sequence:")
+def fibonacci(n):
+    a = 0
+    b = 1
+    if n == 1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+    for i in range(2, n):
+        c = a + b
+        a = b
+        b = c
+        print(c)
+
+x = fibonacci(10)
+print(x)
+
